e991219c12a4c98787146f3b95e86582e025184e
Kiran Venkatappa <quic_kiranv@quicinc.com>
2025-08-12 22:39:36 +0530

    wifi: ath12k: Modularize driver into common and Wi-Fi 7 specific components
    Split the ath12k driver into two kernel modules:
    
     - ath12k.ko for shared logic across multiple targets
     - ath12k_wifi7.ko for Wi-Fi 7 specific configuration and routines
    
    The common module (ath12k.ko) must be loaded prior to any device-specific
    module, as the latter depends on exported symbols from the former.
    
    As part of this restructuring, Wi-Fi 7 specific files are moved into a
    dedicated `wifi7/` directory and built as a separate module. Common
    symbols are exported accordingly, with further adjustments planned
    in upcoming patches to support architecture-dependent separation.
    
    This modularization improves maintainability and scalability by enabling
    clean separation of hardware-specific logic from the shared driver core.
    
                                              +-----------------+
                                              |                 |
                                              |   ath12k.ko     |
                                              |    (common)     |
            +---------------+                 |                 |
            |               |                 +-----------------+
            |   ath12k.ko   | ===========>
            |               |                 +------------------+
            +---------------+                 |                  |
                                              | ath12k_wifi7.ko  |
                                              | (wifi7 family)   |
                                              |                  |
                                              +------------------+
    
    Tested-on: QCN9274 hw2.0 PCI WLAN.WBE.1.3.1-00173-QCAHKSWPL_SILICONZ-1
    Tested-on: WCN7850 hw2.0 PCI WLAN.HMT.1.0.c5-00481-QCAHMTSWPL_V1.0_V2.0_SILICONZ-3
    Tested-on: IPQ5332 hw1.0 AHB WLAN.WBE.1.3.1-00130-QCAHKSWPL_SILICONZ-1
    
    Signed-off-by: Kiran Venkatappa <quic_kiranv@quicinc.com>
    Reviewed-by: Vasanthakumar Thiagarajan <vasanthakumar.thiagarajan@oss.qualcomm.com>
    Reviewed-by: Baochen Qiang <baochen.qiang@oss.qualcomm.com>
    Link: https://patch.msgid.link/20250812-ath12k-mod-v1-10-8c9b0eb9335d@quicinc.com
    Signed-off-by: Jeff Johnson <jeff.johnson@oss.qualcomm.com>

--- drivers/net/wireless/ath/ath12k/pci.c
+++ drivers/net/wireless/ath/ath12k/pci.c
@@ -1193,6 +1193,7 @@ u32 ath12k_pci_read32(struct ath12k_base *ab, u32 offset)
 		ab_pci->pci_ops->release(ab);
 	return val;
 }
+EXPORT_SYMBOL(ath12k_pci_read32);
 
 void ath12k_pci_write32(struct ath12k_base *ab, u32 offset, u32 value)
 {
@@ -1813,6 +1814,7 @@ int ath12k_pci_register_driver(const enum ath12k_device_family device_id,
 
 	return pci_register_driver(pci_driver);
 }
+EXPORT_SYMBOL(ath12k_pci_register_driver);
 
 void ath12k_pci_unregister_driver(const enum ath12k_device_family device_id)
 {
@@ -1823,6 +1825,7 @@ void ath12k_pci_unregister_driver(const enum ath12k_device_family device_id)
 	pci_unregister_driver(&ath12k_pci_family_drivers[device_id]->driver);
 	ath12k_pci_family_drivers[device_id] = NULL;
 }
+EXPORT_SYMBOL(ath12k_pci_unregister_driver);
 
 /* firmware files */
 MODULE_FIRMWARE(ATH12K_FW_DIR "/QCN9274/hw2.0/*");
