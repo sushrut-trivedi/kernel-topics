61dc1aed0fa3e693596fbd909fac669c1ce9cace
Elson Serrao <elson.serrao@oss.qualcomm.com>
2026-01-26 15:38:26 -0800

    FROMLIST: usb: misc: qcom_eud: improve enable_store API
    Currently enable_store() allows operations irrespective of the EUD state,
    which can result in redundant operations. Avoid this by adding duplicate
    state checks to skip requests when EUD is already in the desired state.
    Additionally, improve error handling with explicit logging to provide
    better feedback.
    
    Signed-off-by: Elson Serrao <elson.serrao@oss.qualcomm.com>
    Reviewed-by: Konrad Dybcio <konrad.dybcio@oss.qualcomm.com>
    Link: https://lore.kernel.org/linux-usb/20260126233830.2193816-6-elson.serrao@oss.qualcomm.com/

--- drivers/usb/misc/qcom_eud.c
+++ drivers/usb/misc/qcom_eud.c
@@ -168,18 +168,27 @@ static ssize_t enable_store(struct device *dev,
 	if (kstrtobool(buf, &enable))
 		return -EINVAL;
 
+	/* Skip operation if already in desired state */
+	if (chip->enabled == enable)
+		return count;
+
 	if (enable) {
 		ret = enable_eud(chip);
-		if (!ret)
-			chip->enabled = enable;
-		else
-			disable_eud(chip);
-
+		if (ret) {
+			dev_err(chip->dev, "failed to enable eud\n");
+			return ret;
+		}
 	} else {
 		ret = disable_eud(chip);
+		if (ret) {
+			dev_err(chip->dev, "failed to disable eud\n");
+			return ret;
+		}
 	}
 
-	return ret < 0 ? ret : count;
+	chip->enabled = enable;
+
+	return count;
 }
 
 static DEVICE_ATTR_RW(enable);
